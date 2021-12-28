997ba98991d3d642ee592193e295e99c7bc1ad99
Azrapay <iftay@yahoo.com>
2021-12-28 13:26:03 +0530

    changes

--- socket_server.py
+++ socket_server.py
@@ -1,40 +1,51 @@
 import socket
 import sys
-  
-  
-# specify Host and Port 
-HOST = '' 
-PORT = 6000
+
+def print_output(message):
+    with open('/opt/myoperator-callbackhandler/output.txt', 'a+') as f:
+        try:            
+            f.seek(0)
+            f.write('\n')
+            f.write(message)
+        except Exception as err:
+            f.write(err)
+        finally:
+            f.close()  
+
+if __name__=='__main__':  
+    # specify Host and Port 
+    HOST = '' 
+    PORT = 6000
+        
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   
-try:
-    # With the help of bind() function 
-    # binding host and port
-    soc.bind((HOST, PORT))
-       
-except socket.error as massage:
-      
-    # if any error occurs then with the 
-    # help of sys.exit() exit from the program
-    print('Bind failed. Error Code : ' 
-          + str(massage[0]) + ' Message ' 
-          + massage[1])
-    sys.exit()
-      
-# print if Socket binding operation completed    
-print('Socket binding operation completed')
-   
-# With the help of listening () function
-# starts listening
-soc.listen(9)
+    try:
+        # With the help of bind() function 
+        # binding host and port
+        soc.bind((HOST, PORT))
+        
+    except socket.error as massage:
+        
+        # if any error occurs then with the 
+        # help of sys.exit() exit from the program
+        print_output('Bind failed. Error Code : ' 
+            + str(massage[0]) + ' Message ' 
+            + massage[1])
+        sys.exit()
+        
+    # print if Socket binding operation completed    
+    print_output('Socket binding operation completed')
+    
+    # With the help of listening () function
+    # starts listening
+    soc.listen(9)
 
-while True:
-    conn, address = soc.accept()
-    # print the address of connection
-    print('Connected with ' + address[0] + ':' 
-        + str(address[1]))
-    data = conn.recv(1024)
+    while True:
+        conn, address = soc.accept()
+        # print the address of connection
+        print_output('Connected with ' + address[0] + ':' 
+            + str(address[1]))
+        data = conn.recv(1024)
 
-    print("RECEIVED: %s" % (data.decode('ascii')))
-    conn.close()
+        print_output("RECEIVED: %s" % (data.decode('ascii')))
+        conn.close()
